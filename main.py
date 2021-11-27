a6b870de51a13f6d89cd05f53832bebf85e108d7
Rony Nudelman <rony.nudelman97@gmail.com>
2021-11-27 17:09:53 +0200

    Added maze_99 (it was generated by a script)

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from Utilities import *
 from Experiments import *
 
 
+import AutomaticGenerator
+
+
 if __name__ == "__main__":
+    AutomaticGenerator.prepare_csv_file()
+    # test_robot(WAStartRobot, [0, 1, 2, 3, 4, 5], heuristic=tail_manhattan_heuristic)
+    # test_robot(WAStartRobot, [0, 1, 2, 3, 4, 5], heuristic=center_manhattan_heuristic)
+    test_robot(WAStartRobot, [99], heuristic=tail_manhattan_heuristic)
+    test_robot(WAStartRobot, [99], heuristic=center_manhattan_heuristic)
+
     # test_robot(UniformCostSearchRobot, [0, 1, 2, 3, 4, 5])
     # test_robot(BreadthFirstSearchRobot, [0, 1, 2, 3, 4, 5])
     # solve_and_display(BreadthFirstSearchRobot, 5)
@@ -14,9 +23,9 @@ if __name__ == "__main__":
     # for i in range(6):
     #     w_experiment(i)
     #     print(f"done {i}", flush=True)
-    print("with simple heuristic:")
-    test_robot(WAStartRobot, [3], heuristic=center_manhattan_heuristic)
-    print("with complicated heuristic:")
-    maze_problem = create_problem("maze_3")
-    h = ShorterRobotHeuristic(maze_problem, k=2)
-    test_robot(WAStartRobot, [3], heuristic=h)
+    # print("with simple heuristic:")
+    # test_robot(WAStartRobot, [3], heuristic=center_manhattan_heuristic)
+    # print("with complicated heuristic:")
+    # maze_problem = create_problem("maze_3")
+    # h = ShorterRobotHeuristic(maze_problem, k=2)
+    # test_robot(WAStartRobot, [3], heuristic=h)
